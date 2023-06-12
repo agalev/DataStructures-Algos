81157e6347384b8deff6b23bad9776e87e420d9b
Alexander <alex.galev@yahoo.com>
2023-06-12 09:28:26 -0600

    Updated Progress

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/longest-common-prefix/description/
+def longestCommonPrefix(strs: list) -> str:
+    result = ''
+    if len(strs) == 1:
+        return strs[0]
+    for i in range(len(strs[0])):
+        for j in range(1, len(strs)):
+            if i >= len(strs[j]) or strs[0][i] != strs[j][i]:
+                return result
+        result += strs[0][i]
+    return result
+
+print(longestCommonPrefix(["flower","flow","flight"])) # "fl"
