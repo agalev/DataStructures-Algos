2c3fe9d1fe715a35c7085648ecfb62283f52a64f
Alexander <alex.galev@yahoo.com>
2023-08-04 10:09:36 -0700

    Updated progress

---
+++ UniqueMorseRepresentations.py
@@ -0,0 +1,43 @@
+# https://leetcode.com/problems/unique-morse-code-words/description/
+def uniqueMorseRepresentations(words: list[str]) -> int:
+    hashmap = {
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h":  "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z":"--.."
+    }
+    result = []
+    for i in words:
+        temp = ''
+        for j in i:
+            temp += hashmap[j]
+        if temp not in result:
+            result.append(temp)
+    return len(result)
+
+# Test Cases
+print(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"])) # return 2
+print(uniqueMorseRepresentations(["a"])) # return 1
+print(uniqueMorseRepresentations(["a", "b"])) # return 2
