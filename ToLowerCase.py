b683512ad6092c292ad4c85ba703539ee3f3ee06
Alexander <alex.galev@yahoo.com>
2023-08-05 10:47:51 -0700

    daily dose of one-lining

---
+++ ToLowerCase.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/to-lower-case/submissions/
+def toLowerCase(s: str) -> str:
+    return ''.join([i.lower() for i in s])
+
+# Test Cases
+print(toLowerCase('Hello')) # return 'hello'
+print(toLowerCase('here')) # return 'here'
+print(toLowerCase('LOVELY')) # return 'lovely'
+print(toLowerCase('al&phaBET')) # return 'al&phabet'
