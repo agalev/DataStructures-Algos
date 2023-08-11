a083078d347ffd51d7bbeb9ab3e1b3a7dc8f8594
Alexander <alex.galev@yahoo.com>
2023-08-11 15:41:36 -0700

    Updated Progress

---
+++ FinalString.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/faulty-keyboard/submissions/
+def finalString(s: str) -> str:
+        result = []
+        for i in range(len(s)):
+            if s[i] == 'i':
+                result.reverse()
+            else:
+                result.append(s[i])
+        return ''.join(result)
+
+# Test Cases
+print(finalString('abc')) # return 'abc'
+print(finalString('string')) # return 'rtsng'
