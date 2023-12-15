caa18425d8066d65dee1d23ca4b71e0c3c8017e1
Alexander Galev <alex.galev@yahoo.com>
2023-12-15 14:06:23 -0800

    daily algo

---
+++ Arrays&Hashing/FindWordsContaining.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/find-words-containing-character/
+def findWordsContaining(words: list, x: str) -> list:
+        result = []
+        for index, element in enumerate(words):
+            if x in element:
+                result.append(index)
+        return result
+
+# Test cases
+print(findWordsContaining(["abc","deq","mee","aqq","dkd","ccc"], "a")) # [0,3]
+print(findWordsContaining(["a","b","c"], "z")) # []
